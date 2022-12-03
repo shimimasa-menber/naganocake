cb6d012b975808e2e6caec63bc494d2b7dc9ee49
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2022-12-03 12:47:47 +0000

    [Add naganocake]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins,skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  namespace :admin do
+  resources :genres, only: [:edit, :create, :index, :update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

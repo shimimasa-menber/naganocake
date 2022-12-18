0c5ee6aa339609ab53c0077bbce71f714e30ec74
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2022-12-18 20:30:45 +0000

    [Add naganocake]

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,8 +2,8 @@ class Admin::HomesController < ApplicationController
 
   def top
     @orders = Order.page(params[:page])
-    @customers = @order.customer
-    @order_details = @order.order_detail
+    # @customers = @order.customer
+    # @order_details = @order.order_detail
   end
 
 end

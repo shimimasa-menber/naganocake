36a08b30d7874e2217a9f86c0c9f1fb4abc612aa
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2022-12-25 03:51:52 +0000

    [Add naganocake]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,9 +2,13 @@ class Public::ItemsController < ApplicationController
 
   def index
     @genres = Genre.all
+    @items = Item.page(params[:page])
   end
 
   def show
+    @genres = Genre.all
+    @item = Item.find(params[:id])
+    @cart_item = @item.cart_item
   end
 
 end

0c54d17fa6749266e8ae1986b4b6e303d87130e7
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2023-01-05 12:04:10 +0000

    [Add naganocake]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -16,14 +16,21 @@ class Public::OrdersController < ApplicationController
   def confirm
     @order = Order.new(order_params)
     binding.pry
+    if params[:order][:select_address] == "1"
+
+    elsif params[:order][:select_address] == "2"
+
+    else params[:order][:select_address] == "3"
+
+    end
   end
 
   def complete
   end
-  
-  def order_params
-  params.require(:order).permit(:payment_method)
-  end
-  
-end
 
+  private
+  def order_params
+  params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
+end

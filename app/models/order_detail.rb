dc0aa72a5ff7db29e189ae3a32e7f2c6111e36ca
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2023-01-09 13:02:38 +0000

    [Add naganocake]

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -5,6 +5,10 @@ class OrderDetail < ApplicationRecord
   def with_tax_price
     (price * 1.1).floor
   end
-  
+
+  def subtotal
+    with_tax_price * amount
+  end
+
   enum making_status: { production_not_allowed: 0, waiting_production: 1, production: 2, production_completed: 3 }
 end

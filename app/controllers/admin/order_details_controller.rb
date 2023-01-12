902e499af2567d02b0f8efabc5fe1a8c739fee9c
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2023-01-12 12:19:48 +0000

    [Add naganocake]

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,14 +1,14 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    order_detail = @order.order_detail 
+    order_detail =OrderDetail.find(params[:id])
     order_detail.update(order_detail_params)
-    redirect_to admin_order_path(@order.id)
+    redirect_to request.referer
   end
 
   private
   def order_detail_params
-    params.require(:order_details).permit(:making_status)
+    params.require(:order_detail).permit(:making_status)
   end
 
 end
